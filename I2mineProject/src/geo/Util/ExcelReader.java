9d9ecaf56ba365f5174de611630aeafb193aff5c
ShichaoDong <dongshichao1988@gmail.com>
2013-02-16 20:35:59 +0200

    1)Modify .gitignore file to ignore bin folder. 2) implement simple methods (create workbook, sheet, write data to sheet) in ExcelWriter class. 3) implement methods of reading data from a sheet.

---
+++ I2mineProject/src/geo/Util/ExcelReader.java
@@ -0,0 +1,71 @@
+package geo.Util;
+
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Cell;
+import jxl.CellType;
+import jxl.DateCell;
+import jxl.LabelCell;
+import jxl.NumberCell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class ExcelReader {
+	
+	private Workbook workbook = null;
+	private Sheet sheet = null;
+	
+	public boolean getWorkbook(String workbookName){
+		
+		boolean isWorkbookExist = false;
+		File file = new File(workbookName);
+		try {
+			workbook = Workbook.getWorkbook(file);
+			isWorkbookExist = true;
+		} catch (BiffException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			isWorkbookExist = false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			isWorkbookExist = false;
+		}
+		
+		return isWorkbookExist;
+	}
+	
+	public void initWorkSheet(int position){
+		sheet = workbook.getSheet(position);
+	}
+	
+	public Object getCellValue(int col, int row, CellType type){
+		
+		if(sheet != null){
+			Cell cell = sheet.getCell(col, row);
+			
+			if(CellType.BOOLEAN == type){
+				
+			}else if(CellType.DATE == type){
+				DateCell dc = (DateCell)cell;
+				return dc.getDate();
+			}else if(CellType.EMPTY == type){
+				
+			}else if(CellType.ERROR == type){
+				
+			}else if(CellType.LABEL == type){
+				LabelCell lc = (LabelCell)cell;
+				return lc.getString();
+			}else if(CellType.NUMBER == type){
+				NumberCell nc = (NumberCell)cell;
+				return nc.getValue(); 
+			}
+			
+			return cell.getContents();
+		}
+		
+		return null;
+	}
+}
---
+++ I2mineProject/src/geo/Util/ExcelWriter.java
@@ -0,0 +1,121 @@
+package geo.Util;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+import jxl.write.Number;
+
+public class ExcelWriter {
+	
+	private WritableWorkbook workBook = null;
+	private WritableSheet sheet = null;
+	
+	private boolean createNewWorkbook(String workbookName){
+		File file = new File(workbookName);
+		try {
+			workBook = Workbook.createWorkbook(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean createSheet(String sheetName, int position){
+		sheet = workBook.createSheet(sheetName, position);
+		return true;
+	}
+	
+	public boolean createSheet(String workbookName, String sheetName, int position){
+		boolean isWorkbookCreated = createNewWorkbook(workbookName);
+		if(isWorkbookCreated == false){
+			// workbook creation failed
+			return false;
+		}else{
+			// workbook creation succeeds
+			boolean isSheetCreated = createSheet(sheetName, position);
+			if(isSheetCreated == false){
+				// sheet creation failed
+				return false;
+			}else{
+				// sheet creation succeeds
+				return true;
+			}
+		}
+	}
+	
+	public boolean addLabelToSheet(int col, int row, String value){
+		
+		boolean isAdd = false;
+		Label label = new Label(col, row, value);
+		if(sheet != null){
+			try {
+				sheet.addCell(label);
+				isAdd = true;
+			} catch (RowsExceededException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				isAdd = false;
+			} catch (WriteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				isAdd = false;
+			}
+		}
+		return isAdd;
+	}
+	
+	public boolean addNumberToSheet(int col, int row, double value){
+		
+		boolean isAdd = false;
+		Number num = new Number(col, row, value);
+		if(sheet != null){
+			try {
+				sheet.addCell(num);
+				isAdd = true;
+			} catch (RowsExceededException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				isAdd = false;
+			} catch (WriteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				isAdd = false;
+			}
+			
+		}
+		return isAdd;
+	}
+	
+	public boolean closeWorkbook(){
+		if(workBook == null){
+			return false;
+		}else{
+			try {
+				workBook.write();
+				workBook.close();
+				return true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			} catch (WriteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+		}
+	}
+	
+	public boolean isWorkbookClosed(){
+		return true;
+	}
+	
+}
