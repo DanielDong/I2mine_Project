bb76b6031097dd27b6ebdceb0a0f282b0774c0bb
Shichao Dong <dongshichao1988@gmail.com>
2013-03-21 00:11:26 +0200

    Fine tune grouping algorithm

--- I2mineProject/src/geo/cluster/ClusterTool.java
+++ I2mineProject/src/geo/cluster/ClusterTool.java
@@ -3,7 +3,6 @@ package geo.cluster;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 
 import net.sf.javaml.clustering.Clusterer;
 import net.sf.javaml.clustering.KMeans;
@@ -26,13 +25,15 @@ public class ClusterTool {
 		Dataset data = FileHandler.loadDataset(new File(fileName), numOfWorkphases, delimiter);
         
         int finalClusterNum = 0;
-        boolean isBestResult = true; // by default, current cluster is good enough
-        Dataset[] clusters = null; 
+        boolean isBestResult = false, flagForTwice = false;
+        Dataset[] clusters = null, finalClusters = null; 
         
         System.out.println("data len:"+data.size());
-        		
-        for(int i = 2 ;i < data.size(); i++){
-        	isBestResult = true; 
+        
+        
+        // Start with cluster number of 2 to num_of_workfaces/2 included
+        for(int i = 2 ;i <= data.size()/2; i++){
+        	
         	System.out.println("i:"+i);
         	
         	Clusterer km = new KMeans(i);
@@ -40,42 +41,22 @@ public class ClusterTool {
         	
         	// Check each cluster to see if i clusters give best result
         	for(int j = 0; j < clusters.length; j++){
-        		//output current cluster
-        		for(int ci = 0; ci < clusters[j].size(); ci++){
-        			System.out.println("i:"+i+" "+clusters[j].get(ci));
-        		}
-        		System.out.println("==============");
         		
         		//This cluster only has one value, ignore
         		if(clusters[j].size() == 1){
         			continue;
         		}
         		
-        		//Collect all the row ids of current cluster
+        		//Find maximum distance in a cluster
         		double maxDist = 0;
         		ArrayList<Integer> excludeId = new ArrayList<Integer> ();
-        		for(int id = 0; id < clusters[j].size(); id++){
-        			excludeId.add(clusters[j].get(id).getID());
-        		}
-        		
-        		//Output all row ids in current cluster
-        		System.out.println("all row ids in current cluster:");
-        		for(int ci = 0; ci < excludeId.size(); ci++){
-        			System.out.print(excludeId.get(ci)+" ");
-        		}
-        		
-        		//Sort the row id in ascending order
-        		Collections.sort(excludeId);
-        		
-        		//Find maximum distance in a cluster
-        		for(int it = 0; it < excludeId.size() - 1; it++){
-        			for(int jt = it + 1; jt < excludeId.size(); jt++){
-        				double tmpDist = data.get(excludeId.get(it)).value(excludeId.get(jt));
-        				//excludeId.add(clusters[j].get(it).getID());
-        				maxDist = ( tmpDist> maxDist)? tmpDist:maxDist;
+        		for(int it = 0; it < clusters[j].size() - 1; it++){
+        			for(int jt = 0; jt < clusters[j].size(); jt++){
+        				double tmpDist = data.get(clusters[j].get(it).getID()).value(jt);
+        				excludeId.add(clusters[j].get(it).getID());
+        				maxDist = ( tmpDist> maxDist)? tmpDist:0;
         			}
         		}
-        		
         		
         		System.out.println("max dist:"+maxDist);
         		
@@ -94,26 +75,40 @@ public class ClusterTool {
         		// This cluster is not a good one
         		if(isGoodCluster == true){
         			isBestResult = false;
+        			break;
         		}
         		// This cluster is good enough
-//        		else{
-//        			isBestResult = true;
-//        		}
-        	}
+        		else{
+        			isBestResult = true;
+        		}
+        	}// for: check each cluster
         	
         	if(isBestResult == true){
         		finalClusterNum = i;
-        		break;
+        		finalClusters = clusters;
+        		//break;
         	}
-        }
+        	
+        	// To make sure each time K-means gives best clusters for current cluster number, 
+        	// Each cluster number is carried out twice
+        	if(flagForTwice == false){
+        		i = i - 1;
+        		flagForTwice = true;
+        	}
+        	else{
+        		flagForTwice = false; 
+        	}
+        }// for: try different cluster number
         
-        //System.out.println("best cluster num:"+finalClusterNum);
-        return clusters;
+        System.out.println("best cluster num:"+finalClusterNum);
+        return finalClusters;
 	}/* getClustersOfWorkphases */
 
     public static void main(String[] args) throws Exception {
-    	Dataset[] ds = ClusterTool.getClustersOfWorkphases("workphase.txt",5, "\t");
+    	Dataset[] ds = ClusterTool.getClustersOfWorkphases("workface-distance.txt", 7, "\t");
     	System.out.println("best cluster num:"+ds.length);
+    	for(int i=0;i<ds.length;i++)
+    		System.out.println(ds[i]);
     }/* main */
 
 }
